3bd6ed6c25fcf7bcfc82ff0f42365e0b6b046d86
yudegaki <kogakuin.shunsuke@gmail.com>
2022-09-08 03:21:38 +0900

    complete chapter3-4

--- exercises/ch-3-ex-2/client.js
+++ exercises/ch-3-ex-2/client.js
@@ -39,15 +39,15 @@ var scope = null;
 var refresh_token = 'j2r3oj32r23rmasd98uhjrk2o3i';
 
 app.get('/', function (req, res) {
-	res.render('index', {access_token: access_token, scope: scope, refresh_token: refresh_token});
+	res.render('index', { access_token: access_token, scope: scope, refresh_token: refresh_token });
 });
 
-app.get('/authorize', function(req, res){
+app.get('/authorize', function (req, res) {
 
 	access_token = null;
 	scope = null;
 	state = randomstring.generate();
-	
+
 	var authorizeUrl = buildUrl(authServer.authorizationEndpoint, {
 		response_type: 'code',
 		scope: client.scope,
@@ -55,127 +55,165 @@ app.get('/authorize', function(req, res){
 		redirect_uri: client.redirect_uris[0],
 		state: state
 	});
-	
+
 	console.log("redirect", authorizeUrl);
 	res.redirect(authorizeUrl);
 });
 
-app.get('/callback', function(req, res){
-	
+app.get('/callback', function (req, res) {
+
 	if (req.query.error) {
 		// it's an error response, act accordingly
-		res.render('error', {error: req.query.error});
+		res.render('error', { error: req.query.error });
 		return;
 	}
-	
+
 	var resState = req.query.state;
 	if (resState != state) {
 		console.log('State DOES NOT MATCH: expected %s got %s', state, resState);
-		res.render('error', {error: 'State value did not match'});
+		res.render('error', { error: 'State value did not match' });
 		return;
 	}
 
 	var code = req.query.code;
 
 	var form_data = qs.stringify({
-				grant_type: 'authorization_code',
-				code: code,
-				redirect_uri: client.redirect_uris[0]
-			});
+		grant_type: 'authorization_code',
+		code: code,
+		redirect_uri: client.redirect_uris[0]
+	});
 	var headers = {
 		'Content-Type': 'application/x-www-form-urlencoded',
 		'Authorization': 'Basic ' + encodeClientCredentials(client.client_id, client.client_secret)
 	};
 
-	var tokRes = request('POST', authServer.tokenEndpoint, 
-		{	
+	var tokRes = request('POST', authServer.tokenEndpoint,
+		{
 			body: form_data,
 			headers: headers
 		}
 	);
 
-	console.log('Requesting access token for code %s',code);
-	
+	console.log('Requesting access token for code %s', code);
+
 	if (tokRes.statusCode >= 200 && tokRes.statusCode < 300) {
 		var body = JSON.parse(tokRes.getBody());
-	
+
 		access_token = body.access_token;
 		console.log('Got access token: %s', access_token);
 		if (body.refresh_token) {
 			refresh_token = body.refresh_token;
 			console.log('Got refresh token: %s', refresh_token);
 		}
-		
+
 		scope = body.scope;
 		console.log('Got scope: %s', scope);
 
-		res.render('index', {access_token: access_token, scope: scope, refresh_token: refresh_token});
+		res.render('index', { access_token: access_token, scope: scope, refresh_token: refresh_token });
 	} else {
-		res.render('error', {error: 'Unable to fetch access token, server response: ' + tokRes.statusCode})
+		res.render('error', { error: 'Unable to fetch access token, server response: ' + tokRes.statusCode })
 	}
 });
 
-app.get('/fetch_resource', function(req, res) {
+app.get('/fetch_resource', function (req, res) {
 
 	console.log('Making request with access token %s', access_token);
-	
+
 	var headers = {
 		'Authorization': 'Bearer ' + access_token,
 		'Content-Type': 'application/x-www-form-urlencoded'
 	};
-	
+
 	var resource = request('POST', protectedResource,
-		{headers: headers}
+		{ headers: headers }
 	);
-	
+
 	if (resource.statusCode >= 200 && resource.statusCode < 300) {
 		var body = JSON.parse(resource.getBody());
-		res.render('data', {resource: body});
+		res.render('data', { resource: body });
 		return;
 	} else {
 		/*
 		 * Instead of always returning an error like we do here, refresh the access token if we have a refresh token
 		 */
-		console.log("resource status error code " + resource.statusCode);
-		res.render('error', {error: 'Unable to fetch resource. Status ' + resource.statusCode});
+		console.log("refresh token use")
+		access_token = null;
+		if (refresh_token) {
+			refreshAccessToken(req, res);
+			return;
+		} else {
+			res.render('error', { error: resource.statusCode });
+			return;
+		}
 	}
-	
-	
+
+
 });
 
-var refreshAccessToken = function(req, res) {
+var refreshAccessToken = function (req, res) {
 
 	/*
 	 * Use the refresh token to get a new access token
 	 */
-	
+
+	let form_data = qs.stringify({
+		grant_type: 'refresh_token',
+		refresh_token: refresh_token
+	});
+
+	let headers = {
+		'Content-Type': 'application/x-www-form-urlencoded',
+		'Authorization': 'Basic ' + encodeClientCredentials(client.client_id, client.client_secret)
+	};
+
+	let tokRes = request('POST', authServer.tokenEndpoint, {
+		body: form_data,
+		headers: headers
+	});
+
+	if (tokRes.statusCode >= 200 && tokRes.statusCode < 300) {
+		let body = JSON.parse(tokRes.getBody());
+
+		access_token = body.access_token;
+		if (body.refresh_token) {
+			refresh_token = body.refresh_token;
+		}
+		scope = body.scope;
+
+		res.redirect('/fetch_resource');
+		return;
+	} else {
+		refresh_token = null;
+		res.render('error', { error: 'Unable to refresh token.' });
+		return;
+	}
 };
 
-var buildUrl = function(base, options, hash) {
+var buildUrl = function (base, options, hash) {
 	var newUrl = url.parse(base, true);
 	delete newUrl.search;
 	if (!newUrl.query) {
 		newUrl.query = {};
 	}
-	__.each(options, function(value, key, list) {
+	__.each(options, function (value, key, list) {
 		newUrl.query[key] = value;
 	});
 	if (hash) {
 		newUrl.hash = hash;
 	}
-	
+
 	return url.format(newUrl);
 };
 
-var encodeClientCredentials = function(clientId, clientSecret) {
+var encodeClientCredentials = function (clientId, clientSecret) {
 	return Buffer.from(querystring.escape(clientId) + ':' + querystring.escape(clientSecret)).toString('base64');
 };
 
 app.use('/', express.static('files/client'));
 
 var server = app.listen(9000, 'localhost', function () {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('OAuth Client is listening at http://%s:%s', host, port);
+	var host = server.address().address;
+	var port = server.address().port;
+	console.log('OAuth Client is listening at http://%s:%s', host, port);
 });
- 
+
